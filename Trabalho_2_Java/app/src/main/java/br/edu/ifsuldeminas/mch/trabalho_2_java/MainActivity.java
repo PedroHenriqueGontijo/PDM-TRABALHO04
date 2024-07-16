ac0a6a6629ecf0bcba40aabdf03c894f853c3b1e
pedro Henrique Valentin <118783466+PedroHenriqueGontijo@users.noreply.github.com>
2024-07-16 09:16:46 -0300

    ..

---
+++ Trabalho_2_Java/app/src/main/java/br/edu/ifsuldeminas/mch/trabalho_2_java/MainActivity.java
@@ -0,0 +1,21 @@
+package br.edu.ifsuldeminas.mch.trabalho_2_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
